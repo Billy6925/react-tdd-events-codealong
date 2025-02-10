6a2754b2bad32f9b0cb02c4b761503ea4b503c75
Billy Eskein <billyeskein@gmail.com>
2025-02-10 15:13:29 +0300

    Update src/__tests__/App.test.js

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -2,5 +2,45 @@ import { render, screen } from "@testing-library/react";
 import App from "../App";
 
 import '@testing-library/jest-dom';
+import userEvent from "@testing-library/user-event";
 
 // Code tests here
+test("pizza checkbox is initially unchecked", () => {
+    render(<App/>);
+    const addPepperoni = screen.getByRole("checkbox", {
+        name:/add pepperoni/i,
+    });
+    expect(addPepperoni).not.toBeChecked(); 
+});
+test("toppings appear in the toppings list when checked", ()=> {
+    render(<App/>);
+
+    const addPepperoni = screen.getByRole("checkbox",{name : /add pepperoni/i});
+    userEvent.click(addPepperoni);
+
+    expect(screen.getAllByRole("listitem").length).toBe(2);
+    expect(screen.getByText("Cheese")).toBeInTheDocument(),
+    expect(screen.queryByText("Pepperoni")).toBeInTheDocument();    
+})
+test("the checkbox becomes checked when clicked",()=> {
+    render(<App/>);
+    const addPepperoni = screen.getByRole("checkbox",{name : /add pepperoni/i});
+        userEvent.click(addPepperoni);
+        expect(addPepperoni).toBeChecked();
+})
+
+test("selected topping disappears when checked a second time", ()=> {
+    render(<App/>);
+
+    const addPepperoni = screen.getByRole("checkbox", {name: /add Pepperoni/i});
+    userEvent.click(addPepperoni)
+
+    expect(addPepperoni).toBeChecked();
+    expect(screen.getByText("Cheese")).toBeInTheDocument();
+    expect(screen.getByText("Pepperoni")).toBeInTheDocument();
+
+    userEvent.click(addPepperoni);
+    expect(addPepperoni).not.toBeChecked();
+    expect(screen.getByText("Cheese")).toBeInTheDocument(); 
+    expect(screen.queryByText("Pepperoni")).not.toBeInTheDocument();
+})
